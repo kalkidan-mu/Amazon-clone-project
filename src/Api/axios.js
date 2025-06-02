ac63700aaf2914f657c186d4d2b94d870a22b83a
kalkidan-mu <kalkidan25mulat@gmail.com>
2025-06-02 12:03:10 +0300

    backend url added

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-e2ef2/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-e2ef2/us-central1/api",
+  baseURL: "https://us-central1-clone-e2ef2.cloudfunctions.net/api",
 });
 
 export {axiosInstance}
